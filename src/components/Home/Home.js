3842b918f1ea2f5dba31f3fc4d69ce0de95c9e4a
gavin771 <gavin.samuels10@gmail.com>
2017-10-08 17:44:16 -0500

    added slider section

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,9 +1,39 @@
 import React, { Component } from 'react'
 
+import Jumbotron from '../Jumbotron/Jumbotron'
+const HOME_API = 'http://localhost:3004/home'
+
+
+
 class Home extends Component {
-    render () {
+
+    constructor(props) {
+        super(props);
+
+        this.state = {
+            home: {}
+        }
+    }
+
+    getHomeData = () => {
+        fetch(HOME_API, { method: 'GET' })
+            .then(res => res.json())
+            .then(json => {
+                this.setState({
+                    home: json
+                })
+            })
+    }
+
+    componentDidMount() {
+        this.getHomeData();
+    }
+
+
+    render() {
         return (
             <div>
+                <Jumbotron slides={this.state.home.slider} />
                 Home
             </div>
         )
---
+++ src/components/Jumbotron/Jumbotron.js
@@ -0,0 +1,40 @@
+import React from 'react'
+import Slider from 'react-slick'
+
+const settings = {
+    arrows: true,
+    dots: true,
+    infinte: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1
+}
+
+const generateSlides = ({ slides }) => {
+    if (slides) {
+        return (
+            <Slider {...settings}>
+                {slides.map((slide) => {
+                    return (
+                        <div key={slide.id} className="item-slider" style={{ background: `url(/images/covers/${slide.cover})` }}>
+                            <div className="caption">
+                                <h4>{slide.topic}</h4>
+                                <p>{slide.title}</p>
+                            </div>
+                        </div>
+                    )
+                })}
+            </Slider>
+        )
+    }
+}
+
+const Jumbotron = (props) => {
+    return (
+        <div>
+            {generateSlides(props)}
+        </div>
+    )
+}
+
+export default Jumbotron
