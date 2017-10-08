725b874b6449700734396d7ac6a082b0402137ca
gavin771 <gavin.samuels10@gmail.com>
2017-10-08 12:39:17 -0500

    created Header, Hom & Footer base components

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,20 @@
 import React, { Component } from 'react'
+import { BrowserRouter, Route } from 'react-router-dom'
+
+import Header from './Header/Header'
+import Footer from './Footer/Footer'
+import Home from './Home/Home'
 
 class App extends Component {
-  render () {
+  render() {
     return (
-      <div>
-        
-      </div>
+      <BrowserRouter>
+        <div>
+          <Header />
+          <Route exact path="/" component={Home} />
+          <Footer />
+        </div>
+      </BrowserRouter>
     )
   }
 }
