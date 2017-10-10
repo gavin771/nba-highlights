9b4dc643cd4e70eba8533a67a3ef4c6506807141
gavin771 <gavin.samuels10@gmail.com>
2017-10-09 19:38:57 -0500

    added tiles component

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -2,6 +2,7 @@ import React, { Component } from 'react'
 
 import Jumbotron from '../Jumbotron/Jumbotron'
 import Subscriptions from '../Subscriptions/Subscriptions'
+import Tiles from '../Tiles/Tiles'
 const HOME_API = 'http://localhost:3004/home'
 
 class Home extends Component {
@@ -34,6 +35,7 @@ class Home extends Component {
             <div>
                 <Jumbotron slides={this.state.home.slider} />
                 <Subscriptions />
+                <Tiles tiles={this.state.home.blocks}/>
             </div>
         )
     }
---
+++ src/components/Tiles/Tiles.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import { Link } from 'react-router-dom'
+import Reveal from 'react-reveal'
+import 'animate.css/animate.css'
+
+const Tiles = (props) => {
+
+    const generateBlocks = ({ tiles }) => {
+        if (tiles) {
+            return tiles.map((t) => {
+                return (
+                    <Reveal
+                        effect="animated fadeInUp"
+                        key={t.id}
+                        className={`item ${t.type}`}>
+
+                        <div className="veil"></div>
+
+                        <div className="image" style={{ background: `url(/images/blocks/${t.image}) no-repeat` }}>
+                        </div>
+
+                        <div className="title">
+                            <Link to={t.link}>
+                                {t.title}
+                            </Link>
+                        </div>
+
+                    </Reveal>
+                )
+            })
+        }
+    }
+    console.log(props);
+    return (
+        <div className="home_blocks">
+            {generateBlocks(props)}
+        </div>
+    )
+}
+
+export default Tiles
